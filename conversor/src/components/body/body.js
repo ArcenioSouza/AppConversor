b0adcb5d2a11f216474d7c3f18fd3a0d01cd28ec
Arcenio Souza <arcenio_neto@icloud.com>
2021-09-15 18:10:54 -0300

    Adiciona component body ao App
    Esse commit adiciona o component body com um título e uma imagem no
    background e configurações de css.
    
    Linguagens utilizadas:
    - HTML, CSS, Javascript, JSX.

---
+++ conversor/src/components/body/body.js
@@ -0,0 +1,17 @@
+import React from "react";
+import '../body/body.css';
+
+const Body = () => {
+
+  return(
+    <div className='body-container'>
+      <div className='body-title'>
+        <h1>Conversor de <span id='type-converter'>Medidas</span></h1>
+      </div>
+      <div className='body-form'>
+        
+      </div>
+    </div>
+  )
+}
+export default Body;
--- conversor/src/pages/home/home.js
+++ conversor/src/pages/home/home.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import Navbar from '../../components/navbar/navbar'
-import "./home.css"
+import Body from '../../components/body/body'
+/* import "./home.css" */
 
 const HomePage = () => {
 
   return (
-    <div className = 'container' >
-      <Navbar/> 
-    </div>
+    <>
+      <div className='header'>
+        <Navbar/>
+      </div>
+      <div className='body'>
+        <Body/>
+      </div>     
+    </>
+
   )
 
 }
 
-export default HomePage
+export default HomePage;
