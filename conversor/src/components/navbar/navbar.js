b9bbd2a2a68ff9931d6a56d616e7af7ecc54ec44
Arcenio Souza <arcenio_neto@icloud.com>
2021-09-15 16:48:35 -0300

    Adiciona component navbar
    Esse commit adiciona a navbar ao appconversor já com seu css
    finalizado.
    
    Linguagens utilizadas
    - HTML
    - CSS
    - Javascript
    - JSX

---
+++ conversor/src/components/navbar/navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "../navbar/navbar.css"
+
+const Navbar = () => {
+
+  return(
+    <div className='container'>
+      <div className='wrapper-title'>
+        <h2><span>App</span>Conversor</h2>
+      </div>
+      <div className='wrapper-options'>
+        <div id='options-title'>
+          <h3>Escolha o tipo de conversor</h3>  
+        </div>
+        <div id='options-list'>
+          <ul>
+            <li>Medidas</li>
+            <li>Temperatura</li>
+            <li>Moeda</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ conversor/src/pages/home/home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../../components/navbar/navbar'
+import "./home.css"
+
+const HomePage = () => {
+
+  return (
+    <div className = 'container' >
+      <Navbar/> 
+    </div>
+  )
+
+}
+
+export default HomePage
