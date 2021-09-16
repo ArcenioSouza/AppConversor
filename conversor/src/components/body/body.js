303c8f4ff9067f7591102687fda9cc261a3129c6
Arcenio Souza <arcenio_neto@icloud.com>
2021-09-16 13:32:56 -0300

    Adiciona form ao body do app
    Esse commit adiciona o form do nosso appconversor já com seu css
    configurado.

--- conversor/src/components/body/body.js
+++ conversor/src/components/body/body.js
@@ -9,7 +9,34 @@ const Body = () => {
         <h1>Conversor de <span id='type-converter'>Medidas</span></h1>
       </div>
       <div className='body-form'>
-        
+        <form className='form-wrapper'>
+          <div>
+            <label>De</label>
+            <input id='input-text1' placeholder='Digite o valor'></input>
+            <select>
+              <option id='input-op1'>Selecione uma medida</option>
+              <option id='input-op2'>Quilômetros</option>
+              <option id='input-op3'>Metros</option>
+              <option id='input-op4'>Centimetros</option>
+              <option id='input-op5'>Milimetros</option>
+            </select>
+          </div>
+          <br/> 
+          <div>
+            <label>Para</label>
+              <select>
+                <option id='input-op1'>Selecione uma medida</option>
+                <option id='input-op2'>Quilômetros</option>
+                <option id='input-op3'>Metros</option>
+                <option id='input-op4'>Centimetros</option>
+                <option id='input-op5'>Milimetros</option>
+              </select>
+          </div>
+          <br/>
+          <div>
+            <h2 id='result'>Resultado</h2>
+          </div>        
+        </form>
       </div>
     </div>
   )
